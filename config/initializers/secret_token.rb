8c76e4936427561ca09f3d23dcc5df7d52d97749
Karen <karenkelley11@gmail.com>
2014-04-26 09:43:00 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BuddyBeats::Application.config.secret_key_base = '343a378de4f6b94efd2b27da8291465c4921a811cdaeb77312952ac73a0be37fec9d1bca0dcd6f539b378878254c6ea9f35cf8eb3548ddba76c3ecc226360f59'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BuddyBeats::Application.config.session_store :cookie_store, key: '_buddy_beats_session'
