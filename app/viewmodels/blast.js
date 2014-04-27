34c5b669e0f85487770db169c0caae5d7668f849
alisonberkowitz <alison.berkowitz@students.olin.edu>
2014-04-27 10:56:01 -0400

    adding ajax request to blast spotify playlist

--- app/viewmodels/blast.js
+++ app/viewmodels/blast.js
@@ -10,7 +10,15 @@
         return vm;
 
         function blast() {
-
+        	//get the json object for user
+        	//add playlist to json object
+        	//change playlist in all of the followers to their playlist
+        	$.ajax({
+  				url: 'http://www.sweatinsync.com/api/blast.php?url='+vm.spotifyURL,
+  				success:function(data){
+    				alert(data);
+  					}
+				});
         }
     }
 );
